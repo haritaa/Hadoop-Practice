66136f202143ce2bd2af0668c794e04bd8903cc1
Saravana Kumar Anbarasu <saravana.anbarasu@gmail.com>
2021-07-16 08:42:28 +0530

    pyspark training materials

--- labs/pyspark/pyspark-application/sparkfile.py
+++
@@ -1,7 +0,0 @@
-from pyspark import SparkContext
-from pyspark import SparkFiles
-finddistance = "/home/hadoop/examples_pyspark/finddistance.R"
-finddistancename = "finddistance.R"
-sc = SparkContext("local", "SparkFile App")
-sc.addFile(finddistance)
-print "Absolute Path -> %s" % SparkFiles.get(finddistancename)
